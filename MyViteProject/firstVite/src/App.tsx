376a8f6da7eb8ab9e4ec6785a9acf24816eae1bd
gege <108668351+gelopagutayao@users.noreply.github.com>
2024-04-27 14:14:06 +0800

    commit
    commit

---
+++ MyViteProject/firstVite/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-orange-400">
+      Gelo Pagutayao
+    </h1>
+    <h1 className="text-3xl font-bold underline text-orange-400">
+      2R2
+    </h1>
+    </>
+  )
+}
+
+export default App
